afdeee66c2674dc95264bdce6d0a8a49f68a7931
Stelios <snzouridakis@gmail.com>
2023-03-08 18:57:57 +0200

    Initial Commit

---
+++ InputReader.py
@@ -0,0 +1,45 @@
+import bz2
+
+
+class InputReader:
+
+    def __init__(self):
+        # Read from input file
+        self.file = bz2.BZ2File("managed-object-index.yaml.bz2", "r")
+        self.data = self.file.read().decode('UTF-8')
+        self.file.close()
+
+        # Split data with new line seperator
+        self.data_list = self.data.split('\n')
+        self.objects_dict = {}
+
+    # Create nested dictionary {Object:{Attribute:DataType}}
+
+    def readInputFile(self):
+        for s in self.data_list:
+
+        # All Objects end with ':'. We split that line and take only the object name as Dictionaries key.
+        # We create an empty dictionary as value to that key.
+            if ":" in s:
+                obj_key = s.split(':')
+                attr_dict = {}
+                self.objects_dict[obj_key[0]] = attr_dict
+        # All Attributes start with '- - '. We split that line and take only the attribute name.
+        # We set it as key on the empty dictionary with an empty value waiting for datatype.
+            elif "- - " in s:
+                attr_key = s.split('- - ')
+                attr_dict[attr_key[1]] = None
+        # All Datatypes start with '  - '. We split that line and we take only datatype.
+        # We set it as value on the previous key
+            elif "  - " in s:
+                attr_datatype = s.split('  - ')
+                attr_dict[attr_key[1]] = attr_datatype[1]
+
+        return self.objects_dict
+
+#if __name__ == '__main__':
+
+#    d = InputReader()
+#    data = d.readInputFile()
+#    print(data)
+
---
+++ main.py
@@ -0,0 +1,53 @@
+import uvicorn
+from pydantic import BaseModel
+
+import InputReader
+import requests
+from fastapi import FastAPI
+
+file = InputReader.InputReader()
+data = file.readInputFile()
+
+app = FastAPI()
+bacon_endpoint = "https://baconipsum.com/api/"
+bacon_request = {'type': 'all-meat'}
+
+
+@app.get("/class/{class_name}")
+async def read_class(class_name):
+    return data[class_name]
+
+
+@app.get("/attribute/{attr_name}")
+async def read_attribute(attr_name):
+    class_list = []
+    ret_list = []
+    for class_key, attr_value in data.items():
+        for attr_key, dt_value in attr_value.items():
+            if attr_key == attr_name:
+                datatype = dt_value
+                class_list.append(class_key)
+    ret_list.append(datatype)
+    ret_list.append(class_list)
+    return ret_list
+
+
+@app.get("/datatype/{datatype}")
+async def read_datatype(datatype):
+    ret_list = []
+    for class_key, attr_value in data.items():
+        for attr_key, dt_value in attr_value.items():
+            if dt_value == datatype:
+                ret_list.append(attr_key)
+    return ret_list
+
+
+@app.get("/random")
+def random_bacon():
+    request = requests.get(url=bacon_endpoint, params=bacon_request)
+    response = request.json()
+    return response[0:4]
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True)
